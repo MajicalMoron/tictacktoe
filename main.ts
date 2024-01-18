ec80af2ad50cbd98fc548ec69a08e2283b4ff1ca
MajicalMoron <153124948+MajicalMoron@users.noreply.github.com>
2024-01-18 13:37:13 -0500

    tick-tack-toe

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    led.plotBrightness(x, y, 0)
+    if (x <= 5) {
+        x += 2
+    } else {
+        x = 0
+        if (y <= 5) {
+            x = 0
+            y = 0
+        }
+        y += 2
+    }
+    led.plotBrightness(x, y, 100)
+})
+let y = 0
+let x = 0
+images.createImage(`
+    . # . # .
+    # # # # #
+    . # . # .
+    # # # # #
+    . # . # .
+    `).showImage(0)
 basic.forever(function () {
 	
 })
